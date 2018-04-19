85e07c7fca1959bc1fe3650cdcec32d3eec5b264
Ryan Anderson <randerson1@stetson.edu>
2018-04-18 22:48:58 -0400

    filled all tables, and connected most of them

--- smugglersindex.sql
+++ smugglersindex.sql
@@ -25,6 +25,7 @@ create table manufacturers
   name varchar(40),
   ceo varchar(40),
   affiliation varchar(40),
+  hq varchar(40),
   planet_id int,
   foreign key (planet_id) references planets (id)
 );
@@ -33,40 +34,45 @@ create table manufacturers
 create table ships
 ( id int not null primary key auto_increment,
   model varchar(25),
-  type varchar(25),
+  ship_class varchar(25),
   length decimal (10, 2),
-  speed int,
+  hyperdrive_class int,
   price decimal(15,2),
   quantity int,
+  manufacturer varchar(40),
+  planet varchar(40),
   manufacturer_id int,
   planet_id int,
   foreign key (manufacturer_id) references manufacturers (id),
   foreign key (planet_id) references planets (id)
 );
-
 create table customers
 ( id int not null primary key auto_increment,
   name varchar(25),
   species varchar(25),
   affiliation varchar(45),
   occupation varchar(25),
-  trust BOOL
+  trust bool
 );
-
 create table orders
 ( id int not null primary key auto_increment,
   cost_total decimal(20,2),
-  ship_method varchar(20),
+  shipping_method varchar(40),
   order_status varchar(25),
+  customer varchar(40),
   customer_id int,
+  orders_ships_id int,
   foreign key (customer_id) references customers (id)
 );
-
 create table orders_ships
-( order_id int not null references orders(id),
-  ship_id int not null references ships (id),
+( order_id int not null auto_increment,
+  ship_name varchar(40),
+  ship_id int,
+  customer varchar(40),
   quantity int not null,
-  primary key(order_id, ship_id)
+  customer_id int,
+  foreign key (customer_id) references customers (id),
+  primary key (order_id)
 );
 
 create table sellers
@@ -75,7 +81,8 @@ create table sellers
   name varchar(40),
   species varchar(40),
   affiliation varchar(40),
-  alive bool
+  alive bool,
+  store_id int
 );
 
 
@@ -84,6 +91,9 @@ create table stores
 (
   id int not null primary key auto_increment,
   name varchar(40),
+  location varchar(40),
+  planet varchar(40),
+  owner varchar(40),
   planet_id int,
   seller_id int,
   foreign key (seller_id) references sellers (id),
--- test.php
+++ test.php
@@ -14,6 +14,164 @@ $sql = 'insert into planets (name, system, affiliation) values ("Hoth", "Outer R
 $stmt = DB::run($sql);
 echo "$sql" . "\n";
 
+//add planets *********
+// add Corellia
+$sql = 'insert into planets (name, system, affiliation) values ("Corellia", "Corellia", "New Republic")';
+$stmt = DB::run($sql);
+$sql = 'insert into planets (name, system, affiliation) values ("Selenia", "Corellia", "New Republic")';
+$stmt = DB::run($sql);
+$sql = 'insert into planets (name, system, affiliation) values ("Mandalore", "Mandalore", "Mandalore Protectors")';
+$stmt = DB::run($sql);
+$sql = 'insert into planets (name, system, affiliation) values ("Killian", "Fuller", "Black Star Confederacy")';
+$stmt = DB::run($sql);
+$sql = 'insert into planets (name, system, affiliation) values ("Kuat", "Kuat", "New Republic")';
+$stmt = DB::run($sql);
+$sql = 'insert into planets (name, system, affiliation) values ("Coruscant", "Coruscant", "New Republic")';
+$stmt = DB::run($sql);
+$sql = 'insert into planets (name, system, affiliation) values ("Jakku", "Jakku", "Independent")';
+$stmt = DB::run($sql);
+$sql = 'insert into planets (name, system, affiliation) values ("Tatooine", "Tatoo", "Hutt Clan")';
+$stmt = DB::run($sql);
+//planets done**********
+
+//add manufacturers ******
+//add kuat drive yards
+$sql = 'insert into manufacturers (name, ceo, hq, affiliation) values ("Corellian Engineering Corp", "Corran Destt", "Corellia", "New Republic")';
+$stmt = DB::run($sql);
+//add corellian engineering systems
+$sql = 'insert into manufacturers (name, ceo, hq, affiliation) values ("Kuat Drive Yards", "Kuat of Kuat", "Kuat", "Kuat Systems")';
+$stmt = DB::run($sql);
+//add ManalMotors
+$sql = 'insert into manufacturers (name, ceo, hq, affiliation) values ("MandalMotors", "Karric Nayms", "Mandalore", "Mandalorian Protectors")';
+$stmt = DB::run($sql);
+//link manufacturers' planet_id to planets table using name field
+$sql = 'update manufacturers 
+          inner join planets 
+            on (manufacturers.hq = planets.name) 
+              set manufacturers.planet_id = planets.id';
+$stmt = DB::run($sql);
+// Manufacturers done ****
+
+//add ships
+//add YT-1300
+$sql = 'insert into ships (model, manufacturer, ship_class, length, price, quantity, planet, hyperdrive_class) 
+                  values ("YT-1300","Corellian Engineering Corp", "subcapital", 34.75, 100000, 72, "Corellia", 2)';
+$stmt = DB::run($sql);
+$sql = 'insert into ships (model, manufacturer, ship_class, length, price, quantity, planet, hyperdrive_class) 
+                  values ("YT-2400","Corellian Engineering Corp", "small", 18.65, 130000, 31, "Selenia", 2)';
+$stmt = DB::run($sql);
+$sql = 'insert into ships (model, manufacturer, ship_class, length, price, quantity, planet, hyperdrive_class) 
+                  values ("Star Destroyer","Kuat Drive Yards", "executor", 19000, 71000000000, 3, "Coruscant", 2)';
+$stmt = DB::run($sql);
+$sql = 'update ships 
+          inner join planets 
+            on (ships.planet = planets.name) 
+              set ships.planet_id = planets.id';
+$stmt = DB::run($sql);
+$sql = 'update ships 
+          inner join manufacturers 
+            on (ships.manufacturer = manufacturers.name) 
+              set ships.manufacturer_id = manufacturers.id';
+$stmt = DB::run($sql);
+
+//add stores
+//Add Unkar's concession stand
+$sql = 'insert into stores (name, location, planet, owner) 
+          values ("Unkar Plutt\'s Concession Stand", "Niima Outpost", "Jakku", "Unkar Plutt")';
+$stmt = DB::run($sql);
+$sql = 'insert into stores (name, location, planet, owner) 
+          values ("Wald\'s Parts", "Mos Espa", "Tatooine", "Wald")';
+$stmt = DB::run($sql);
+$sql = 'insert into stores (name, location, planet, owner) 
+          values ("Shipwright Auxiliary Spaceport", "Blastfield Shipyards", "Corellia", "S-4V5")';
+$stmt = DB::run($sql);
+$sql = 'update stores 
+          inner join planets 
+            on (stores.planet = planets.name) 
+              set stores.planet_id = planets.id';
+$stmt = DB::run($sql);
+
+//add sellers
+$sql = 'insert into sellers (name, species, affiliation, alive) 
+          values ("Unkar Plutt", "Crolute", "Independent", true)';
+$stmt = DB::run($sql);
+$sql = 'insert into sellers (name, species, affiliation, alive) 
+          values ("Wald", "Rodian", "Independent", true)';
+$stmt = DB::run($sql);
+$sql = 'insert into sellers (name, species, affiliation, alive) 
+          values ("Watto", "Toydarian", "Independent", false)';
+$stmt = DB::run($sql);
+$sql = 'insert into sellers (name, species, affiliation, alive) 
+          values ("S-4V5", "Droid", "New Republic", false)';
+$stmt = DB::run($sql);
+//update sellers with their owned stores
+$sql = 'update sellers
+          inner join stores
+            on (sellers.name = stores.owner)
+              set sellers.store_id = stores.id';
+$stmt = DB::run($sql);
+//update stores with seller info
+$sql = 'update stores
+          inner join sellers
+            on (stores.owner = sellers.name)
+              set stores.seller_id = sellers.id';
+$stmt = DB::run($sql);
+
+//add orders
+$sql = 'insert into orders (cost_total, shipping_method, order_status, customer) 
+          values (260000, "economy", "shipped", "Dash Rendar")';
+$stmt = DB::run($sql);
+$sql = 'insert into orders (cost_total, shipping_method, order_status, customer) 
+          values (100000, "overnight", "archived", "Lando Calrissian")';
+$stmt = DB::run($sql);
+$sql = 'insert into orders (cost_total, shipping_method, order_status, customer) 
+          values (284000000000, "intergalactic economy", "delivered", "Bob Snoke")';
+$stmt = DB::run($sql);
+
+//add customers
+$sql = 'insert into customers (name, species, affiliation, occupation, trust) 
+          values ("Dash Rendar", "human", "independent", "smuggler", true)';
+$stmt = DB::run($sql);
+$sql = 'insert into customers (name, species, affiliation, occupation, trust) 
+          values ("Lando Calrissian", "human", "independent", "administrator", false)';
+$stmt = DB::run($sql);
+$sql = 'insert into customers (name, species, affiliation, occupation, trust) 
+          values ("Bob Snoke", "humanoid", "The New Order", "community organizer", true)';
+$stmt = DB::run($sql);
+
+//update orders with their customers
+$sql = 'update orders
+          inner join customers
+            on (orders.customer = customers.name)
+              set orders.customer_id = customers.id';
+$stmt = DB::run($sql);
+
+
+//add order_ships
+$sql = 'insert into orders_ships (ship_name, customer, quantity) 
+          values ("YT-2400", "Dash Rendar", 2)';
+$stmt = DB::run($sql);
+$sql = 'insert into orders_ships (ship_name, customer, quantity) 
+          values ("YT-1300", "Lando Calrissian", 1)';
+$stmt = DB::run($sql);
+$sql = 'insert into orders_ships (ship_name, customer, quantity) 
+          values ("Star Destroyer", "Bob Snoke", 4)';
+$stmt = DB::run($sql);
+
+//add cust id to orders
+$sql = 'update orders
+          inner join customers
+            on (orders.customer = customers.name)
+              set orders.customer_id = customers.id';
+$stmt = DB::run($sql);
+
+//add orders_ships to orders
+$sql = 'update orders
+          inner join orders_ships
+            on (orders.customer = orders_ships.customer)
+              set orders.orders_ships_id = orders_ships.order_id';
+$stmt = DB::run($sql);
+
 //2. Select * from planets
 $sql = "select * from planets";
 $stmt = DB::run($sql);
